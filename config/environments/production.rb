e9c812ff4514ab06361e8b99a8b0db9825966884
allisonjulian <masjulian13@gmail.com>
2016-05-16 20:01:11 +0800

    Debugging...email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,14 +82,14 @@ Rails.application.configure do
   config.assets.compile = true
 
   config.action_mailer.default_url_options = { host: 'https://sleepy-gorge-13163.herokuapp.com' }
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #     :address              => "smtp.gmail.com",
-  #     :port                 => 587,
-  #     :domain: "gmail.com",
-  #     :user_name            => mary.julian@obf.ateneo.edu,
-  #     # :password             => ENV["GMAIL_PASSWORD"],
-  #     :authentication       => :plain,
-  #     :enable_starttls_auto => true
-  # }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain: "gmail.com",
+      :user_name            => "mary.julian@obf.ateneo.edu",
+      :password             => ENV["GMAIL_PASSWORD"],
+      :authentication       => :plain,
+      :enable_starttls_auto => true
+  }
 end
