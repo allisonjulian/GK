6426ebcc3b3f93eda76b034b6d7c00e8e64ce75a
allisonjulian <masjulian13@gmail.com>
2016-05-16 19:52:10 +0800

    Debugging...email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
       :port                 => 587,
       :domain: "gmail.com",
       :user_name            => mary.julian@obf.ateneo.edu,
-      :password             => ENV["GMAIL_PASSWORD"],
+      # :password             => ENV["GMAIL_PASSWORD"],
       :authentication       => :plain,
       :enable_starttls_auto => true
   }
